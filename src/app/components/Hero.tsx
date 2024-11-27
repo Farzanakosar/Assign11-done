366bea604a10adce33263f19acec0b3f676577e1
unknown <fatimaazeem711@gmail.com>
2024-11-28 02:11:18 +0500

    2nd commit

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,14 +4,15 @@ function Hero() {
   return (
     <>
     <div className='flex flex-col justify-center items-center md:items-stretch md:flex-wrap lg:flex-row lg:w-full'>
-      <div className='bg-red-500 text-yellow-100 h-[160px] w-full md:h-80 lg:flex-1 text-center  font-bold'>Farzana Kosar</div>
-      </div>
+      <div className='bg-red-500 text-yellow-100 h-[160px] w-full lg:h-80 lg:flex-1 text-center font-bold'>Farzana Kosar</div>
+      
       
       <div className="flex flex-col md:flex-row w-full md:w-full lg:flex-1 lg:flex-row lg:w-auto">
 
       <div className='bg-green-500 text-yellow-100 h-40 w-full md:h-80 lg:h-80 lg:flex-1  text-center font-bold'>Farzana Kosar</div>
       <div className='bg-blue-500 text-yellow-100 h-40 w-full md:h-80 lg:h-80 lg:flex-1 text-center font-bold'>Farzana Kosar</div>
 
+      </div>
       </div>
      
       </>
